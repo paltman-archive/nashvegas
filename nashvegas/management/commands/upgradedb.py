565f7cc08c1cd6eb679913b2ed5afcdd6fa1e272
Patrick Altman <patrick@studionow.com>
2009-02-21 00:27:34 -0600

    initial commit

---
+++ nashvegas/management/commands/upgradedb.py
@@ -0,0 +1,11 @@
+"""
+Execute SQL Scripts / Upgrade Database
+======================================
+
+"""
+
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    pass
+
