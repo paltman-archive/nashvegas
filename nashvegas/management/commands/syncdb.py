26a35ef30eb9af03f1f7eef517d6e64100511b43
Jannis Leidel <jannis@leidel.info>
2011-01-17 16:17:31 +0100

    Added custom syncdb command that overrides the core syncdb command when nashvegas is installed and prompts for confirmation before running syncdb since upgradedb should be used instead.

---
+++ nashvegas/management/commands/syncdb.py
@@ -0,0 +1,25 @@
+from django.core.management.commands.syncdb import Command as SyncDBCommand
+
+
+class Command(SyncDBCommand):
+    """
+    A custom syncdb command that asks you for confirmation
+    before syncing the database.
+    """
+    
+    def handle_noargs(self, **options):
+        if options.get('interactive'):
+            confirm = raw_input("""
+You have requested a database sync.
+This CONFLICTS WITH NASHVEGAS.
+Are you sure you want to do this?
+
+Type 'yes' to continue, or 'no' to cancel: """)
+        else:
+            confirm = 'yes'
+        
+        if confirm == 'yes':
+            super(Command, self).handle_noargs(**options)
+        else:
+            print "Sync cancelled."
+
