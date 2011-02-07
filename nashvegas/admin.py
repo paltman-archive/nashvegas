6f4dc82db27d39072aefa1d6525c25439dec0ebb
Patrick Altman <paltman@gmail.com>
2011-02-06 22:36:18 -0600

    added a workable admin

--- nashvegas/admin.py
+++ nashvegas/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from nashvegas.models import Migration
 
 
-admin.site.register(Migration)
+class MigrationAdmin(admin.ModelAdmin):
+    list_display = ["migration_label", "date_created", "scm_version"]
+    list_filter = ["date_created"]
+    search_fields = ["content", "migration_label"]
+
+
+admin.site.register(Migration, MigrationAdmin)
