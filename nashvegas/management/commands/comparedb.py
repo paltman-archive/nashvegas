0b331f2fa9072e69efca5b24787aaf5a1ad64a3d
jpic <jamespic@gmail.com>
2012-03-07 14:45:06 +0100

    Fixed comparedb, needs some testing thought because apparently i'm way behind the diff it outputs or the output is not relevant

--- nashvegas/management/commands/comparedb.py
+++ nashvegas/management/commands/comparedb.py
@@ -50,9 +50,11 @@ class Command(BaseCommand):
         
         command = "pg_dump -s %s" % connections[self.db].settings_dict["NAME"]
         if NASHVEGAS and "pg_dump" in settings.NASHVEGAS:
-            command = settings.NASHVEGAS["pg_dump"]
+            command = "%s -s %s" % (settings.NASHVEGAS["pg_dump"], 
+                connections[self.db].settings_dict["NAME"])
         
         print "Getting schema for current database..."
+        print command
         current_sql = Popen(command.split(), stdout=PIPE).stdout.readlines()
         
         print "Getting schema for fresh database..."
@@ -61,7 +63,9 @@ class Command(BaseCommand):
         connections[self.db].close()
         connections[self.db].settings_dict["NAME"] = self.name
         call_command("syncdb", interactive=False, verbosity=0)
-        new_sql = Popen(command.split(), stdout=PIPE).stdout.readlines()
+        print command
+        new_sql = Popen(command.split() + ["-s", self.name], 
+            stdout=PIPE).stdout.readlines()
         connections[self.db].close()
         connections[self.db].settings_dict["NAME"] = orig
         self.teardown_database()
