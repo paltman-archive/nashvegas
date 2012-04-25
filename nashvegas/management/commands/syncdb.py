ab211d0f43436d81a300a7e8227fa95aa2844b63
David Cramer <dcramer@gmail.com>
2012-04-25 15:32:20 -0700

    Correct syncdb behavior

--- nashvegas/management/commands/syncdb.py
+++ nashvegas/management/commands/syncdb.py
@@ -6,7 +6,11 @@ class Command(SyncDBCommand):
     def handle_noargs(self, **options):
         # Run migrations first
         # {'load_initial_data': False, 'verbosity': 1, 'interactive': False, 'database': 'default'}
-        call_command("upgradedb", do_execute=True, database=options.get('database'), verbosity=options.get('verbosity'))
+        if options.get('database'):
+            databases = [options.get('database')]
+        else:
+            databases = None
+        call_command("upgradedb", do_execute=True, databases=databases, verbosity=options.get('verbosity'))
 
         # Follow up with a syncdb on anything that wasnt included in migrations
         # (this catches things like test-only models)
