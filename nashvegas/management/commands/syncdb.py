d68bdfe0b89137efc6b0c167663a0edf7decb4cd
Patrick Altman <paltman@gmail.com>
2012-07-17 20:43:37 -0500

    Update style to be consistent with project

--- nashvegas/management/commands/syncdb.py
+++ nashvegas/management/commands/syncdb.py
@@ -5,18 +5,18 @@ from django.core.management.commands.syncdb import Command as SyncDBCommand
 class Command(SyncDBCommand):
     def handle_noargs(self, **options):
         # Run migrations first
-        if options.get('database'):
-            databases = [options.get('database')]
+        if options.get("database"):
+            databases = [options.get("database")]
         else:
             databases = None
-
+        
         call_command("upgradedb",
             do_execute=True,
             databases=databases,
-            interactive=options.get('interactive'),
-            verbosity=options.get('verbosity'),
+            interactive=options.get("interactive"),
+            verbosity=options.get("verbosity"),
         )
-
+        
         # Follow up with a syncdb on anything that wasnt included in migrations
         # (this catches things like test-only models)
         super(Command, self).handle_noargs(**options)
