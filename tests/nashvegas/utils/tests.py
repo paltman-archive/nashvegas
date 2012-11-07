c04492c5aac600c404e47656176a29c2a9ead0d0
David Cramer <dcramer@gmail.com>
2012-11-07 15:39:49 -0800

    Add basic test for duplicates in get_pending_migrations

--- tests/nashvegas/utils/tests.py
+++ tests/nashvegas/utils/tests.py
@@ -1,7 +1,7 @@
+import mock
 from django.test import TestCase
 from nashvegas.utils import get_capable_databases, get_all_migrations, \
-  get_file_list
-
+  get_file_list, get_pending_migrations
 from os.path import join, dirname
 
 mig_root = join(dirname(__import__('tests', {}, {}, [], -1).__file__), 'fixtures', 'migrations')
@@ -43,3 +43,26 @@ class GetAllMigrationsTest(TestCase):
         self.assertEquals(len(other), 2)
         self.assertTrue((1, join(path, 'other', '0001.sql')) in other)
         self.assertTrue((2, join(path, 'other', '0002_bar.sql')) in other)
+
+
+class GetPendingMigrationsTest(TestCase):
+    @mock.patch('nashvegas.utils.get_all_migrations')
+    @mock.patch('nashvegas.utils.get_applied_migrations')
+    def test_handles_duplicate_migration_numbers(self, get_applied_migrations, get_all_migrations):
+        get_applied_migrations.return_value = {
+            'dupes': ['0001.sql', '0002_foo.sql'],
+        }
+        get_all_migrations.return_value = {
+            'dupes': [(1, '0001.sql'), (2, '0002_bar.sql'), (2, '0002_foo.sql')],
+        }
+
+        path = join(mig_root, 'multidb')
+        results = dict(get_pending_migrations(path))
+
+        get_applied_migrations.assert_called_once_with(None)
+        get_all_migrations.assert_called_once_with(path, None)
+
+        self.assertEquals(len(results), 1)
+        self.assertTrue('dupes' in results)
+        self.assertEquals(len(results['dupes']), 1)
+        self.assertTrue('0002_bar.sql' in results['dupes'])
